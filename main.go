86e901ccf1deb7e1f444aac6ac41090779f5dc66
Anthony Allen <anthony.webit@gmail.com>
2024-05-20 10:42:41 -0500

    Added autocert to api

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/autotls"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from your Go microservice!")
+	fmt.Fprintf(w, "Hello from your Go microservice!")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Server is running on port 8080")
-    http.ListenAndServe(":8080", nil)
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello from your Go microservice!",
+		})
+	})
+
+	m := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("anthony-allen.com"),
+		Cache:      autocert.DirCache("cache/"),
+	}
+
+	log.Fatal(autotls.RunWithManager(r, &m))
 }
