0f18ea63e79bf058a23748dc08e69572b1f54d04
Anthony Allen <anthony.webit@gmail.com>
2024-05-20 18:29:02 -0500

    Put code back to run on 8080

--- main.go
+++ main.go
@@ -1,28 +1,19 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
-
-	"github.com/gin-gonic/autotls"
-	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/acme/autocert"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
 func main() {
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello from the Go microservice!",
-		})
-	})
-
-	m := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("anthony-allen.com"),
-		Cache:      autocert.DirCache("cache/"),
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("ListenAndServe:", err)
 	}
-
-	log.Fatal(autotls.RunWithManager(r, &m))
 }
